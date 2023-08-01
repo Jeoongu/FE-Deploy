6b5d9b3077f6a1bb2342559bdb295cf3feda60bf
Jeoongu <ms235280@naver.com>
2023-08-01 19:50:07 +0900

    환경변수 설정

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
                 </div>
                 <div className="card back">
                     <div>
-                        <span className="name">박정우</span>
-                        <span className="position">BE</span>
+                        <span className="name">{process.env.REACT_APP_NAME}</span>
+                        <span className="position">{process.env.REACT_APP_PART}</span>
                     </div>
-                    <a className="github" href="https://github.com/Jeoongu"></a>
+                    <a className="github" href={process.env.REACT_APP_GITHUB}></a>
                 </div>
             </div>
         </>
