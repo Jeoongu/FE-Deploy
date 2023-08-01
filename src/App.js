c9ba9605589f1880a722c9541cfdd1c06e800ae3
Jeoongu <ms235280@naver.com>
2023-08-01 19:24:07 +0900

    프로필 명함

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
                 </div>
                 <div className="card back">
                     <div>
-                        <span className="name">숭멋사</span>
-                        <span className="position">FE</span>
+                        <span className="name">박정우</span>
+                        <span className="position">BE</span>
                     </div>
-                    <a className="github"></a>
+                    <a className="github" href="https://github.com/Jeoongu"></a>
                 </div>
             </div>
         </>
